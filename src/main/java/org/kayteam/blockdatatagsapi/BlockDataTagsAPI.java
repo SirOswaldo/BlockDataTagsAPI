693997bd0eee3fb7e72126e9da4543f9b327f3a8
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-01-12 01:15:26 -0300

    Start Project

---
+++ src/main/java/org/kayteam/blockdatatagsapi/BlockDataTagsAPI.java
@@ -0,0 +1,16 @@
+package org.kayteam.blockdatatagsapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockDataTagsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
